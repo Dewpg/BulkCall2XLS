0591196b933502a4ad44b6b5cc9efd4b06c36e9a
_Dew <34615139+Dewpg@users.noreply.github.com>
2023-07-20 18:34:40 -0500

    Bulk as first sheet
    Updated to make sure Bulk file is the first items on the sheet.  Need to insert a row beneath the first row on the first sheet to align all the data.

--- main.py
+++ main.py
@@ -28,35 +28,69 @@ def add_txt_to_excel(excel_file):
         wb = openpyxl.Workbook()
 
     # Get the correct sheet or create it if not exists
-    first_sheet_name = extract_sheet_name(txt_files[0])
+    first_txt_file = None
+    for txt_file in txt_files:
+        if "BULK" in txt_file.upper():
+            first_txt_file = txt_file
+            break
+
+    if not first_txt_file:
+        print("No file with 'BULK' in its filename found.")
+        return
+
+    first_sheet_name = extract_sheet_name(first_txt_file)
     if first_sheet_name not in wb.sheetnames:
         sheet = wb.create_sheet(title=first_sheet_name)
     else:
         sheet = wb[first_sheet_name]
 
-    # Process each .txt file and write its contents to the sheet in the Excel file
+    # Process the first .txt file and write its contents to the sheet in the Excel file
+    first_txt_path = os.path.join(script_folder, first_txt_file)
+    if first_txt_file.endswith('.xls'):
+        xls_data = xlrd.open_workbook(first_txt_path)
+        sheet_data = xls_data.sheet_by_index(0)
+        headers = sheet_data.row_values(0)
+        data = [sheet_data.row_values(i) for i in range(sheet_data.nrows)]
+    else:
+        df = pd.read_csv(first_txt_path, delimiter='\t', dtype=str, low_memory=False)
+        data = df.values.tolist()
+        headers = df.columns.tolist()
+
+    # Calculate the starting column based on existing data on the sheet
+    start_column = sheet.max_column + 1 if sheet.max_column is not None else 1
+
+    # Set the starting row to 1 for the first .txt file
+    start_row = 1
+
+    # Append the contents to the sheet for the first file (include the first column)
+    for index, row in enumerate(data):
+        for col_index, value in enumerate(row):
+            sheet.cell(row=start_row + index, column=start_column + col_index, value=value)
+
+    # Process subsequent .txt files and append their contents to the right of the first file
     for txt_file in txt_files:
-        txt_path = os.path.join(script_folder, txt_file)
-        if txt_file.endswith('.xls'):
-            xls_data = xlrd.open_workbook(txt_path)
-            sheet_data = xls_data.sheet_by_index(0)
-            headers = sheet_data.row_values(0)
-            data = [sheet_data.row_values(i) for i in range(1, sheet_data.nrows)]
-            df = pd.DataFrame(data, columns=headers)
-        else:  # Assuming it's a .txt file with tab-delimited data
-            # Specify data types and set low_memory=False to prevent DtypeWarning
-            df = pd.read_csv(txt_path, delimiter='\t', dtype=str, low_memory=False)
+        if txt_file != first_txt_file:
+            txt_path = os.path.join(script_folder, txt_file)
+            if txt_file.endswith('.xls'):
+                xls_data = xlrd.open_workbook(txt_path)
+                sheet_data = xls_data.sheet_by_index(0)
+                data = [sheet_data.row_values(i) for i in range(sheet_data.nrows)]
+            else:
+                df = pd.read_csv(txt_path, delimiter='\t', dtype=str, low_memory=False)
 
-        # Calculate the starting column based on existing data on the sheet
-        start_column = sheet.max_column + 1 if sheet.max_column is not None else 1
+                # Remove the first column for subsequent files
+                df = df.iloc[:, 1:]
 
-        # Set the starting row to 1 for each .txt file
-        start_row = 1
+                # Convert the DataFrame to a list of lists to match the .xls data structure
+                data = df.values.tolist()
 
-        # Append the contents to the sheet, excluding the first column for subsequent files
-        for index, row in df.iterrows():
-            for col_index, value in enumerate(row):
-                sheet.cell(row=start_row + index, column=start_column + col_index, value=value)
+            # Append the contents to the sheet for subsequent files (exclude the first column)
+            for index, row in enumerate(data):
+                for col_index, value in enumerate(row):
+                    sheet.cell(row=start_row + index, column=start_column + col_index, value=value)
+
+            # Update the starting column for subsequent files
+            start_column = sheet.max_column + 1
 
     # Save the changes to master.xlsx
     wb.save(excel_file)
