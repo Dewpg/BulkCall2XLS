542100aa4760f7a20a370b46189f5dde4ba84a17
_Dew <34615139+Dewpg@users.noreply.github.com>
2023-07-21 11:22:35 -0500

    Update main.py
    get rid of uneccesary save in the middle of the main function.

--- main.py
+++ main.py
@@ -7,12 +7,27 @@ def clean_sheet_name(sheet_name):
     # Remove characters that cannot be used in a worksheet name
     return "".join(c for c in sheet_name if c.isalnum() or c in [' ', '_', '-'])
 
-def delete_empty_columns(sheet):
-    # Find the columns that have an empty value in the first row
-    empty_columns = set(col_idx for col_idx in range(1, sheet.max_column + 1) if not sheet[get_column_letter(col_idx) + '1'].value)
+def delete_empty_and_duplicate_columns(sheet):
+    # Find the columns that have an empty value in the first row or are duplicates
+    first_row_values = set()
+    duplicate_columns = set()
 
-    # Delete the empty columns in reverse order to avoid index issues
-    for col_idx in sorted(empty_columns, reverse=True):
+    for col_idx in range(1, sheet.max_column + 1):
+        col_letter = get_column_letter(col_idx)
+        cell_value = sheet[col_letter + '1'].value
+
+        if not cell_value:
+            # Mark empty columns for deletion
+            duplicate_columns.add(col_idx)
+        elif cell_value in first_row_values:
+            # Mark duplicate columns for deletion
+            duplicate_columns.add(col_idx)
+        else:
+            # Add the value to the set of seen values in the first row
+            first_row_values.add(cell_value)
+
+    # Delete the marked columns in reverse order to avoid index issues
+    for col_idx in sorted(duplicate_columns, reverse=True):
         sheet.delete_cols(col_idx)
 
 def find_empty_column(sheet):
@@ -91,19 +106,9 @@ def main():
         print("Finished datatyping a file") 
         # Increment the starting column for the next file
         start_column += len(data[0])
-    
-    # Save the changes to master.xlsx
-    wb.save('master.xlsx')
-    
-    print("Deleting Blank Columns")
-    # Open the xlsx document again
-    wb = openpyxl.load_workbook('master.xlsx')
 
-    # Get the sheet
-    sheet = wb[sheet_name]
-
-    # Delete empty columns from the sheet
-    delete_empty_columns(sheet)
+    # Delete empty and duplicate columns from the sheet
+    delete_empty_and_duplicate_columns(sheet)
 
     # Save the workbook again
     wb.save('master.xlsx')
