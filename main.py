ef7a4e06e062536a46a2626815dc03466ce8191f
_Dew <34615139+Dewpg@users.noreply.github.com>
2023-07-21 10:26:58 -0500

    Deletes empty columns now
    Works as intended.  After inserting blank row beneath first several columns from first sheet. PIVOT works for whole sheet.

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ def clean_sheet_name(sheet_name):
     # Remove characters that cannot be used in a worksheet name
     return "".join(c for c in sheet_name if c.isalnum() or c in [' ', '_', '-'])
 
+def delete_empty_columns(sheet):
+    # Find the columns that are empty (all cells in the column are None or empty strings)
+    empty_columns = set(col_idx for col_idx in range(1, sheet.max_column + 1) if all(not sheet[get_column_letter(col_idx) + str(row)].value for row in range(1, sheet.max_row + 1)))
+
+    # Delete the empty columns in reverse order to avoid index issues
+    for col_idx in sorted(empty_columns, reverse=True):
+        sheet.delete_cols(col_idx)
+
+def find_empty_column(sheet):
+    # Find the first empty column in the sheet
+    for col in range(1, sheet.max_column + 2):
+        col_letter = get_column_letter(col)
+        if all(not sheet[col_letter + str(row)].value for row in range(1, sheet.max_row + 1)):
+            return col
+    return sheet.max_column + 2
+
 def main():
     # Get all filenames that end with ".txt"
     filenames = [f for f in os.listdir('.') if f.endswith('.txt')]
@@ -25,6 +41,7 @@ def main():
     else:
         wb = openpyxl.Workbook()
 
+    print("Creating dated sheetname as second sheet in file")
     # Create a worksheet with the name of the last 8 characters before the .txt for the BULK file
     sheet_name = bulk_filename[-12:-4]
     sheet_name = clean_sheet_name(sheet_name)  # Clean the sheet name
@@ -32,9 +49,10 @@ def main():
         sheet = wb.create_sheet(title=sheet_name)
     else:
         sheet = wb[sheet_name]
-
+    print("Sheet named " + sheet_name)
+    
     # Read the data from the BULK file
-    with open(bulk_filename, 'r', encoding='latin-1') as f:
+    with open(bulk_filename, 'r') as f:
         reader = csv.reader(f, delimiter='\t')
         data = list(reader)
 
@@ -47,39 +65,49 @@ def main():
             if cell.isnumeric():
                 sheet[col_letter + str(row + 1)] = int(cell)
             else:
-                sheet[col_letter + str(row + 1)] = cell
-
-    # Get the starting column based on existing data on the sheet
-    start_column = sheet.max_column + 1 if sheet.max_column is not None else 1
+                sheet[col_letter + str(row + 1)] = cell 
+    
+    # Get the starting column based on existing data on the sheet or the first empty column
+    start_column = find_empty_column(sheet)
 
     # Iterate through the rest of the TXT files and add the data to the right
     for filename in filenames:
-        with open(filename, 'r', encoding='latin-1') as f:
+        with open(filename, 'r') as f:
             reader = csv.reader(f, delimiter='\t')
             data = list(reader)
-
-        # Write the data to the existing sheet
+        print("Adding " + filename)
+        print("Checking to validate value type as number")
+        # Write the data to the existing sheet starting from the first empty column
         for row, line in enumerate(data):
-            for col, cell in enumerate(line[1:]):  # Skip the first column
+            for col, cell in enumerate(line):
                 col_letter = get_column_letter(start_column + col)  # Convert the column index to letter
-
+                
                 # Check if the cell value is a number and set the data type accordingly
                 if cell.isnumeric():
-                    sheet[col_letter + str(row + 2)].data_type = 'n'
-                    sheet[col_letter + str(row + 2)].value = int(cell)
+                    sheet[col_letter + str(row + 1)].data_type = 'n'
+                    sheet[col_letter + str(row + 1)].value = int(cell)
                 else:
-                    sheet[col_letter + str(row + 2)] = cell
-
+                    sheet[col_letter + str(row + 1)] = cell
+        print("Finished datatyping a file") 
         # Increment the starting column for the next file
-        start_column += len(data[0]) - 1
-
-    # Delete empty columns
-    columns_to_delete = [col for col in range(1, sheet.max_column + 1) if not any(sheet.cell(row=row, column=col).value for row in range(1, sheet.max_row + 1))]
-    for col in reversed(columns_to_delete):
-        sheet.delete_cols(col)
-
+        start_column += len(data[0])
+    
     # Save the changes to master.xlsx
     wb.save('master.xlsx')
+    
+    print("Deleting Blank Columns")
+    # Open the xlsx document again
+    wb = openpyxl.load_workbook('master.xlsx')
 
+    # Get the sheet
+    sheet = wb[sheet_name]
+
+    # Delete empty columns from the sheet
+    delete_empty_columns(sheet)
+
+    # Save the workbook again
+    wb.save('master.xlsx')
+    print("Deletions processed")
+    
 if __name__ == '__main__':
     main()
